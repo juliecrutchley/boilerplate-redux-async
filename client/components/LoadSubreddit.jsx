1131ba5170734b8c0fa100c42708820110adecd8
EDA <eda@mac-mini-1.local>
2017-06-05 14:30:46 +1200

    make loadsubreddit comp stateful and add search function based on search term entered

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -2,10 +2,41 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
 
-let LoadSubreddit = ({dispatch}) => (
-  <button
-    onClick={() => dispatch(fetchPosts('newzealand'))}
-    >Fetch Posts</button>
-)
+class LoadSubreddit extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      subreddit: ''
+    }
+    this.fieldChanged = this.fieldChanged.bind(this)
+  }
+
+  fieldChanged (e) {
+    console.log(this.state)
+    this.setState({
+      subreddit: e.target.value
+    })
+  }
+
+  render () {
+    return (
+      <div className='LoadSubreddit'>
+        <input placeholder='enter subreddit here' name='subreddit' onChange={e => this.fieldChanged(e)} value={this.state.subreddit} />
+
+        <button className='button' onClick={() => this.props.dispatch(fetchPosts(this.state.subreddit))}>Fetch Posts</button>
+      </div>
+    )
+  }
+}
 
 export default connect()(LoadSubreddit)
+
+// let LoadSubreddit = ({dispatch}) => (
+//   <div>
+//     <input type='text' placeholder='enter subreddit here' name='subreddit' onchange={e => this.fieldChanged(e)} value={this.state.subreddit} />
+//
+//     <button className='button' onClick={() => dispatch(fetchPosts('newzealand'))}>Fetch Posts</button>
+//   </div>
+// )
+
+    // <input placeholder='enter subreddit here' name='name' onChange={e => this.fieldChanged(e)} value={this.state.name} />
